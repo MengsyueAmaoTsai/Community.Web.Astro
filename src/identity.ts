cb1dcebe7fe17e59844a8a273026caac0586b9a8
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-08-18 05:43:19 +0800

    AuthorizedUser component

--- src/identity.ts
+++ src/identity.ts
@@ -1,16 +1,24 @@
 interface ICurrentUser {
 	id: string;
+	name: string;
 	email: string;
 	isAuthenticated: boolean;
 }
 
 class CurrentUser implements ICurrentUser {
 	id: string;
+	name: string;
 	email: string;
 	isAuthenticated: boolean;
 
-	public constructor(id: string, email: string, isAuthenticated: boolean) {
+	public constructor(
+		id: string,
+		name: string,
+		email: string,
+		isAuthenticated: boolean,
+	) {
 		this.id = id;
+		this.name = name;
 		this.email = email;
 		this.isAuthenticated = isAuthenticated;
 	}
@@ -18,6 +26,7 @@ class CurrentUser implements ICurrentUser {
 
 export const currentUser = new CurrentUser(
 	"UID0009994",
+	"Community User",
 	"community@richillcapital.com",
-	false,
+	true,
 ) as ICurrentUser;
