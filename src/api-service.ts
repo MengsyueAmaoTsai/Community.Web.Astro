7b8369ed34b1baa20e3259b01a9d0f69097b84ab
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-08-18 04:09:37 +0800

    Add ApiService

---
+++ src/api-service.ts
@@ -0,0 +1,28 @@
+type TradingAppResponse = { id: string };
+
+type SignalSourceResponse = { id: string };
+
+const BASE_ADDRESS = "http://localhost:11000";
+
+interface IApiService {
+	listTradingApps(): Promise<TradingAppResponse[]>;
+	listSignalSources(): Promise<SignalSourceResponse[]>;
+}
+
+class ApiService implements IApiService {
+	public listTradingApps = async (): Promise<TradingAppResponse[]> => {
+		throw new Error("Method not implemented.");
+	};
+
+	public listSignalSources = async (): Promise<SignalSourceResponse[]> => {
+		const response = await fetch(`${BASE_ADDRESS}/api/v1/signal-sources`);
+
+		if (!response.ok) {
+			throw new Error("Failed to fetch signal sources");
+		}
+
+		return response.json();
+	};
+}
+
+export const apiService = new ApiService() as IApiService;
