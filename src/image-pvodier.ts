5755d558961f5c67c5f08b89f9244d84e9180841
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-08-18 22:48:42 +0800

    BrandLogoLink

---
+++ src/image-pvodier.ts
@@ -0,0 +1,7 @@
+export class ImageProvider {
+	public static readonly MicrosoftLogoLight =
+		"https://apps.microsoft.com/assets/icons/microsoft-logo-light-1.png";
+
+	public static readonly StoreLogo =
+		"https://apps.microsoft.com/assets/icons/microsoft-logo-light-2.png";
+}
