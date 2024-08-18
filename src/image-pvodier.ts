cbb40dd2cb696d2a20f8eb3aa80ea2da012706d1
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-08-18 23:27:12 +0800

    Add store button

--- src/image-pvodier.ts
+++ src/image-pvodier.ts
@@ -4,4 +4,7 @@ export class ImageProvider {
 
 	public static readonly StoreLogo =
 		"https://apps.microsoft.com/assets/icons/microsoft-logo-light-2.png";
+
+	public static readonly SmallLogo =
+		"	https://apps.microsoft.com/assets/icons/dark-logo-32x32.png";
 }
