9e76b5697fe662ae682ff57aa18e59213a6789b8
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-08-18 19:37:40 +0800

    reset

---
+++ src/app-info.ts
@@ -0,0 +1,6 @@
+class AppInfo {
+	public static readonly id = "RichillCapital.Community.Web";
+	public static readonly name = "Richill Capital Community";
+	public static readonly description =
+		"Provides license purchasing and download services for trading platform scripts, along with signal subscription and copy trading functionalities.";
+}
--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,3 @@
-export const SITE_TITLE = "Richill Capital Community";
-
-export const SITE_DESCRIPTION =
-	"A blog about software development and other things.";
-
 export const NAVIGATION_ITEMS = [
 	{
 		title: "Home",
