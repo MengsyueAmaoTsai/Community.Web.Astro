84634a009a8f9f7c995f8b2d874c8bb939e0fa45
Mengsyue Amao Tsai <mengsyue.tsai@outlook.com>
2024-08-18 11:11:57 +0800

    refactor

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,160 @@
 export const SITE_TITLE = "Richill Capital Community";
+
 export const SITE_DESCRIPTION =
 	"A blog about software development and other things.";
+
+export const NAVIGATION_ITEMS = [
+	{
+		title: "Home",
+		route: "/",
+	},
+	{
+		title: "Trading apps",
+		route: "/trading-apps",
+	},
+	{
+		title: "Signal sources",
+		route: "/signal-sources",
+	},
+	{
+		title: "About",
+		route: "/about",
+	},
+];
+
+export const FOOTER_MENU_GROUPS = [
+	{
+		title: "What's New",
+		items: [
+			{
+				title: "More content in the Microsoft Store",
+				route: "/",
+			},
+			{
+				title: "Apps",
+				route: "/",
+			},
+			{
+				title: "Games",
+				route: "/",
+			},
+			{
+				title: "Movies & TV",
+				route: "/",
+			},
+			{
+				title: "Windows Experience Blog",
+			},
+		],
+	},
+	{
+		title: "Microsoft Store",
+		items: [
+			{
+				title: "Microsoft account",
+				route: "/",
+			},
+			{
+				title: "Microsoft Store support",
+				route: "/",
+			},
+			{
+				title: "Returns",
+				route: "/",
+			},
+			{
+				title: "Flexible payments",
+				route: "/",
+			},
+			{
+				title: "Policies and Code of Conduct",
+				route: "/",
+			},
+		],
+	},
+	{
+		title: "For Developers",
+		items: [
+			{
+				title: "Publish your app",
+				route: "/",
+			},
+			{
+				title: "Advertise with us",
+				route: "/",
+			},
+			{
+				title: "Generate your app badge",
+				route: "/",
+			},
+		],
+	},
+	{
+		title: "Windows",
+		items: [
+			{
+				title: "Careers",
+				route: "/",
+			},
+			{
+				title: "About Microsoft",
+				route: "/",
+			},
+			{
+				title: "Company news",
+				route: "/",
+			},
+			{
+				title: "Investors",
+				route: "/",
+			},
+			{
+				title: "Diversity and inclusion",
+				route: "/",
+			},
+			{
+				title: "Accessibility",
+				route: "/",
+			},
+			{
+				title: "SecuritySustainability",
+				route: "/",
+			},
+		],
+	},
+];
+
+export const FOOTER_LINKS = [
+	{
+		title: "Contact Microsoft",
+		route: "/",
+	},
+	{
+		title: "Privacy",
+		route: "/",
+	},
+	{
+		title: "Consumer Health Privacy",
+		route: "/",
+	},
+	{
+		title: "Terms of use",
+		route: "/",
+	},
+	{
+		title: "Trademarks",
+		route: "/",
+	},
+	{
+		title: "Safety & eco",
+		route: "/",
+	},
+	{
+		title: "Recycling",
+		route: "/",
+	},
+	{
+		title: "About our ads",
+		route: "/",
+	},
+];
